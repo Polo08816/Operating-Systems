cad53419f51feb567d013f37d1732a3582533628
Kevin Kuo <kkuo12887@gmail.com>
2015-09-19 15:45:08 -0400

    Updating code for opening, reading, and closing file.

--- Homeworks/Homework2/Homework2/hello.c
+++ Homeworks/Homework2/Homework2/hello.c
@@ -1,10 +1,32 @@
 /* Hello World program */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 main()
 {
-    printf("Hello World");
+    FILE *ifp, *ofp;
+	
+	printf("Hello World");	
 
+	char *mode = "r";
+	char outputFilename[] = "out.list";
 
+	ifp = fopen("input.txt", "r");
+
+	if (ifp == NULL){
+		fprintf(stderr, "Can't open input file in.list!\n");
+		exit(1);
+	}
+
+	ofp = fopen(outputFilename, "w");
+
+	if (ofp == NULL) {
+		 fprintf(stderr, "Can't open output file %s!\n",
+         outputFilename);
+		exit(1);
+	}
+
+	fclose(ifp);
+	fclose(ofp);
 }
