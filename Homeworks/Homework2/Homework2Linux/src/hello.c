f54e6da71c4fe4d66c65ee447283207cd968bf04
Kevin Kuo <kkuo12887@gmail.com>
2015-09-28 19:35:53 -0400

    Adding initial project settings and source code.

---
+++ Homeworks/Homework2/Homework2Linux/src/hello.c
@@ -0,0 +1,42 @@
+/* Hello World program */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+
+	char *outputFilename = "output.txt";
+	char ch;
+
+    FILE *ifp, *ofp;
+
+	printf("Hello World");
+
+	ifp = fopen("input.txt", "r");
+
+	if (ifp == NULL){
+		fprintf(stderr, "Can't open input file in.list!\n");
+		exit(1);
+	}
+
+	ofp = fopen(outputFilename, "w");
+
+	if (ofp == NULL) {
+		fprintf(stderr, "Can't open output file %s!\n",
+        outputFilename);
+		exit(1);
+	}
+
+	while (1) {
+      ch = fgetc(ifp);
+
+      if (ch == EOF)
+         break;
+      else
+         putc(ch, ofp);
+	}
+
+	fclose(ifp);
+	fclose(ofp);
+}
