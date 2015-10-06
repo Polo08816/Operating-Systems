964051e731c8f348da547dcc986a5e10f02b25f1
Kevin Ku <kkuo12887@gmail.com>
2015-10-06 15:30:38 -0400

    Fixing back to what I want.

--- Homeworks/Homework2/Homework2Linux/src/hello.c
+++ Homeworks/Homework2/Homework2Linux/src/hello.c
@@ -2,28 +2,17 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<sys/types.h>
-
-#define MAX_COUNT = 200
-#define BUF_SIZE = 100
 
 int main()
 {
-
-	pid_t pid;
 
 	char *outputFilename = "output.txt";
 	char ch;
 
 	FILE *ifp, *ofp;
+//	FILE *ifp;
 
-	pid = getpid();
-	printf("\nParent process PID: %d", pid);
-
-	fork();
-	pid = getpid();
-
-	printf("\nHello World!  My PID is: %d", pid);
+	printf("Hello World");
 
 	ifp = fopen("input.txt", "a");
 
@@ -39,7 +28,7 @@ int main()
         outputFilename);
 		exit(1);
 	}
-l
+
 	while (1) {
       ch = fgetc(ifp);
 
