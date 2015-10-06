6dcf7f1865563aaa4bd7d801ab4d8bbdea64d201
Kevin Kuo <kkuo12887@gmail.com>
2015-10-06 15:29:11 -0400

    Fixing Homework 2.

--- Homeworks/Homework2/Homework2Linux/src/hello.c
+++ Homeworks/Homework2/Homework2Linux/src/hello.c
@@ -2,17 +2,28 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/types.h>
+
+#define MAX_COUNT = 200
+#define BUF_SIZE = 100
 
 int main()
 {
+
+	pid_t pid;
 
 	char *outputFilename = "output.txt";
 	char ch;
 
 	FILE *ifp, *ofp;
-//	FILE *ifp;
 
-	printf("Hello World");
+	pid = getpid();
+	printf("\nParent process PID: %d", pid);
+
+	fork();
+	pid = getpid();
+
+	printf("\nHello World!  My PID is: %d", pid);
 
 	ifp = fopen("input.txt", "a");
 
@@ -28,7 +39,7 @@ int main()
         outputFilename);
 		exit(1);
 	}
-
+l
 	while (1) {
       ch = fgetc(ifp);
 
