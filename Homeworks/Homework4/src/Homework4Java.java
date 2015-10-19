7dc2e95b18f8bca28e4e963b3294b3afdd5159c5
Kevin Ku <kkuo12887@gmail.com>
2015-10-18 20:48:24 -0400

    Adding Homework4 in Java.

---
+++ Homeworks/Homework4/src/Homework4Java.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kevin Kuo
+ *
+ */
+public class Homework4Java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
