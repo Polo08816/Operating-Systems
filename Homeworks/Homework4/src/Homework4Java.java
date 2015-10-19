5459a4a801b9d8112c2e0a04446368414b6fc788
Kevin Ku <kkuo12887@gmail.com>
2015-10-18 21:39:03 -0400

    Started code for Homework 4.

--- Homeworks/Homework4/src/Homework4Java.java
+++ Homeworks/Homework4/src/Homework4Java.java
@@ -6,14 +6,24 @@
  * @author Kevin Kuo
  *
  */
-public class Homework4Java {
+public class Homework4Java implements Runnable{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < 20; i++){
+			(new Thread(new Homework4Java())).start();
+		}
+		
+	}
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		long threadId = Thread.currentThread().getId();
+		System.out.println("Hello World! Thread ID: " + threadId);
 	}
 
 }
